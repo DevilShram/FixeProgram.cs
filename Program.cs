c9b4f3cd1579b19896b4538eb6ae77ce35f0f424
DevilShram <konahina.len@yandex.ru>
2023-09-25 20:19:51 +0300

    first commit(fixe)

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача №2
+
+// Console.Write("Введите число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// if ( a < b)
+//     Console.WriteLine(b);
+// else 
+//      Console.WriteLine(a);
+
+// Задача №4
+
+//  Console.Write("Введите число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число: ");
+// int c = Convert.ToInt32(Console.ReadLine());
+// int max =a;
+// if (max < b) max = b;
+// if (max < c) max = c;
+// Console.Write("Ответ: ");
+// Console.WriteLine(max);
+
+// Задача №6
+
+//  Console.Write("Введите число: ");
+//  int a = Convert.ToInt32(Console.ReadLine());
+
+//  if (a % 2 == 0)
+//     Console.WriteLine($"{a} является чётным ");
+//  else
+//     Console.WriteLine($"{a} не является чётным ");
+ 
+//  Задача №8
+
+//  Console.Write("Введите число: ");
+//  int n = int.Parse(Console.ReadLine());
+//  int i = 2;
+
+// while (n >= i)
+// {
+//      Console.Write(i + " ");
+//      i = i + 2;
+// }
