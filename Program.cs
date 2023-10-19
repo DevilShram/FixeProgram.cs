d047f7849abf396a3671caaad41be6227c22dd41
DevilShram <konahina.len@yandex.ru>
2023-10-19 11:13:14 +0300

    add №41 and №43

--- Program.cs
+++ Program.cs
@@ -131,3 +131,51 @@
 // Console.Write("Введите число: ");
 // int n = Convert.ToInt32(Console.ReadLine());
 // Console.WriteLine(f(n));
+
+// Задача №41
+
+// Console.Write("Введите массив: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int[] Array = new int[n];
+
+// void mas(int n)
+// {
+// for (int i = 0; i < n; i++)
+// {
+
+// Console.WriteLine($"Введите {i+1} элемент массива ");
+// Array[i] = Convert.ToInt32(Console.ReadLine());
+// }
+
+// }
+// int kol(int[] Array)
+// {
+// int i=0;
+// int sum = 0;
+// while (i < Array.Length)
+// {
+// if(Array[i]>0)
+// sum = sum + 1;
+// i = i + 1;
+// }
+// return sum;
+// }
+// mas(n);
+// Console.Write($"{kol(Array)}");
+
+// задача №43
+
+// Console.WriteLine("Введите переменную b1: ");
+// double b1 = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Введите переменную k1: ");
+// double k1 = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Введите переменную b2: ");
+// double b2 = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Введите переменную k2: ");
+// double k2 = Convert.ToDouble(Console.ReadLine());
+
+// double x = -(b1 - b2) / (k1 - k2);
+// double y = k1 * x + b1;
+
+// Console.Write($"Точка пересечения двух прямых: [{x},{y}]");
+
